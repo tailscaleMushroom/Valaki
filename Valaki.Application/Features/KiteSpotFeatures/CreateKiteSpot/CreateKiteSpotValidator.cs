218e902db2331f6c1198133629d038c059d9ec66
koningsvisser <leijsvogel@proton.me>
2024-05-23 10:24:08 +0200

    add

---
+++ Valaki.Application/Features/KiteSpotFeatures/CreateKiteSpot/CreateKiteSpotValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valaki.Application.Features.KiteSpotFeatures.CreateKiteSpot
+{
+    public sealed class CreateKiteSpotValidator : AbstractValidator<CreateKiteSpotRequest>
+    {
+        public CreateKiteSpotValidator() 
+        {
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
---
+++ Valaki.Application/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Valaki.Application.Common.Behaviors;
+
+namespace Valaki.Application
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        }
+    }
+}
