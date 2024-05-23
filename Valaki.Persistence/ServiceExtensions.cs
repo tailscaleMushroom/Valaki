0b54e51b0692074e154747649b96be83eb77b9c2
koningsvisser <leijsvogel@proton.me>
2024-05-23 10:36:51 +0200

    add

---
+++ Valaki.Persistence/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Valaki.Application.Repositories;
+using Valaki.Persistence.Context;
+using Valaki.Persistence.Repositories;
+
+namespace Valaki.Persistence
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Sqlite");
+            services.AddDbContext<DataContext>(opt => opt.UseSqlite(connectionString));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IKiteSpotRepository, KiteSpotRepository>();
+        }
+    }
+}
