aa4a1dd3bae8d5383a0442d0d4f7213c80d9c8ee
koningsvisser <leijsvogel@proton.me>
2024-05-23 10:59:39 +0200

    add

---
+++ Valaki.Api/Controllers/KiteSpotController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Valaki.Application.Features.KiteSpotFeatures.CreateKiteSpot;
+
+namespace Valaki.Api.Controllers;
+
+[ApiController]
+[Route("kitespot")]
+public class KiteSpotController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public KiteSpotController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CreateKiteSpotResponse>> Create(CreateKiteSpotRequest request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return Ok(response);
+    }
+}
+
